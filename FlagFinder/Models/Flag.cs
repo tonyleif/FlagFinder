8f88e0dae1553699a38c1d10e20992882afa1705
Anthony Leif <tonyleif@hotmail.com>
2017-07-30 22:50:42 -0500

    Pages creates. Button selectors controlled by jquery. Models made. Just a few flags added for samples.

---
+++ FlagFinder/Models/Flag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlagFinder.Models
+{
+    public class Flag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageLocation { get; set; }
+        public bool Red { get; set; }
+        public bool Orange { get; set; }
+        public bool Yellow { get; set; }
+        public bool Green { get; set; }
+        public bool Blue { get; set; }
+        public bool Purple { get; set; }
+        public bool Brown { get; set; }
+        public bool White { get; set; }
+        public bool Black { get; set; }
+    }
+}
---
+++ FlagFinder/Models/Flags.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlagFinder.Models
+{
+    public class Flags : CollectionBase
+    {
+        public Flags()
+        {
+        }
+
+        public void LoadFlags()
+        {
+            Add("Canada", true, false, false, false, false, false, false, true, false);
+            Add("Mexico", true, true, true, true, true, false, true, true, false );
+            Add("United States", true, false, false, false, true, false, false, true, false);
+        }
+
+        void Add(string name, bool red, bool orange, bool yellow, bool green, bool blue, bool purple, bool brown, bool white, bool black)
+        {
+            Flag flag = new Flag();
+            flag.Name = name;
+            flag.Red = red;
+            flag.Orange = orange;
+            flag.Yellow = yellow;
+            flag.Green = green;
+            flag.Blue = blue;
+            flag.Purple = purple;
+            flag.Brown = brown;
+            flag.White = white;
+            flag.Black = black;
+            //flag.ImageLocation = "~/Content/Images/" + imgName + ".jpg";
+            List.Add(flag);
+        }
+
+    }
+}
